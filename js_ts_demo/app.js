ceab6923f0b69cd0e078a074dc9496db7f9174e2
synergy2411 <synergy2411@outlook.com>
2019-02-18 17:42:23 +0530

    app.js incl

---
+++ js_ts_demo/app.js
@@ -0,0 +1,60 @@
+// var x = "Some String";
+
+// console.log(typeof x);
+
+// x = 10;
+
+// console.log(typeof x);
+
+// var x = "20";
+// var y = 10;
+
+
+// console.log(x - y);     //  10
+// console.log(x + y);     //  2010
+
+// var x = "20";
+// var y = 20;
+// console.log(x===y);      //false
+
+
+//Functions 
+// var addition = function(num1 , num2){
+//     return num1 + num2;
+// }
+
+// console.log(addition(2,4));
+
+// function mystery(){
+//     var chooseNumber = function (){
+//         return 7;
+//     }
+//     return chooseNumber;
+//     var chooseNumber = function (){ 
+//         return 12;
+//     }
+// }
+
+// var nestedFunc = mystery();
+
+// console.log(nestedFunc());     //   ?
+
+//Closure
+
+// function demo(){
+//     var x = 4;
+//     return function(){
+//         return ++x; 
+//     }
+// }
+
+// var nestedFunc = demo();
+// console.log(nestedFunc());      //5
+// console.log(nestedFunc());      //
+// console.log(nestedFunc());      //
+
+
+// var arr = [true, 32, "String", function(){console.log("Hello")}, ["Foo", "Bar"]];
+
+// console.log(arr[3]);
+
